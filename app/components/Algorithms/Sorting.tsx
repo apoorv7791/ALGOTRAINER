2f2ca6bb390661f02982a584f27aa62cf3ed1da5
Apoorv Singh <singhapoorv7791@gmail.com>
2025-10-27 09:58:44 +0530

    The Algorithms folder and the screens have been created

---
+++ app/components/Algorithms/Sorting.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { useTheme } from '@/app/Themes/Themecontext';
+
+const Sorting = () => {
+    const { theme } = useTheme();
+    return (
+        <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
+            <Text style={[styles.header, { color: theme.colors.text }]}>Sorting Algorithms</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    }
+})
+
+export default Sorting;
