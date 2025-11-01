fcedad8e52f25d857aae49336b2c8a1912e8e9e2
Apoorv Singh <singhapoorv7791@gmail.com>
2025-11-01 17:31:08 +0530

    Modified _layout.tsx in Algorithms folder

--- app/Algorithms/_layout.tsx
+++ app/Algorithms/_layout.tsx
@@ -7,9 +7,12 @@ const AlgorithmsLayout = () => {
             <Stack.Screen name="Sorting" />
             <Stack.Screen name="Searching" />
             <Stack.Screen name="Greedy" />
+            <Stack.Screen name="DynamicProgramming" />
+            <Stack.Screen name="GraphAlgorithms" />
             <Stack.Screen name="Backtracking" />
         </Stack>
     );
+
 };
 
 export default AlgorithmsLayout;
