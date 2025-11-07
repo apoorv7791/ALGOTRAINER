eba6006bd022f3ee89b7f20670b9ffd9f8f7ce57
Apoorv Singh <singhapoorv7791@gmail.com>
2025-11-07 15:05:37 +0530

    Deleted two folders and modfies the layout one

--- app/AdvancePatterns/_layout.tsx
+++ app/AdvancePatterns/_layout.tsx
@@ -23,26 +23,6 @@ const AdvancePatternsLayout = () => {
                 name="SlidingWindow"
                 options={{ title: 'Sliding Window Pattern' }}
             />
-            <Stack.Screen
-                name="FastAndSlowPointers"
-                options={{ title: 'Fast and Slow Pointers Pattern' }}
-            />
-            <Stack.Screen
-                name="BinarySearch"
-                options={{ title: 'Binary Search Pattern' }}
-            />
-            <Stack.Screen
-                name="PrefixSum"
-                options={{ title: 'Prefix Sum Pattern' }}
-            />
-            <Stack.Screen
-                name="BacktrackingPatterns"
-                options={{ title: 'Backtracking Pattern' }}
-            />
-            <Stack.Screen
-                name="Bit Manipulation"
-                options={{ title: 'Bit Manipulation Pattern' }}
-            />
         </Stack>
     );
 }
