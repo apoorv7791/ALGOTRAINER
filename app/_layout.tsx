633092b606931b4b13e73bb4f5e5e3aa7b75cfe2
Apoorv Singh <singhapoorv7791@gmail.com>
2026-01-17 20:02:05 +0530

    modified:   app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,10 @@
 import React from 'react';
 import { ThemeProvider } from './Themes/ThemeContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { LogBox } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router/stack';
 
 export default function RootLayout() {
-    // Temporarily silence the deprecated SafeAreaView warning coming from
-    // dependencies. Prefer fixing/upgrading those packages long-term.
-    LogBox.ignoreLogs(['SafeAreaView has been deprecated']);
     return (
         <SafeAreaProvider>
             <GestureHandlerRootView style={{ flex: 1 }}>
