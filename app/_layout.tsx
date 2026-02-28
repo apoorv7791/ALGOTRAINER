333819350a991a197e68a51a8648f2fdb277f1c6
Apoorv Singh <singhapoorv7791@gmail.com>
2026-02-28 14:14:38 +0530

    modified:   app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,14 @@ import { ThemeProvider } from './Themes/ThemeContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router/stack';
+import { LogBox } from 'react-native';
+
+// Suppress specific deprecation warnings from third-party libraries
+LogBox.ignoreLogs([
+    'SafeAreaView has been deprecated',
+    'InteractionManager has been deprecated',
+    'Property "opacity" of AnimatedComponent'
+]);
 
 export default function RootLayout() {
     return (
